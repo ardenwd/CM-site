623e9428f67f11d3738b222ccfbacc163fb46c9c
ardenwd <96889434+ardenwd@users.noreply.github.com>
2022-09-16 22:47:45 -0400

    Add files via upload

---
+++ Dj.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: DJ
+*/
+
+get_header();
+
+ ?>
+ <head>
+   <!-- add fonts here -->
+   <title>DJ</title>
+ </head>
+
+<body class="dj">
+  <div class="container">
+
+    <!--home buton -->
+    <a href="../observations">
+      <div class="return-home return-home-dj">
+        <div class="return-home-color"></div>
+        <div class="return-home-text">Home</div>
+      </div>
+    </a>
+
+    <div class="grid">
+
+      <a href="https://open.spotify.com/album/3a9qH2VEsSiOZvMrjaS0Nu?si=Y-fKPI4VRYqxvHFW_EcmFA" class="a00 grid-element">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/art/charles.jpg" class="g-img">
+      </a>
+
+      <a href="https://open.spotify.com/playlist/7ELg4mOokKLSJfKYCBZLB2" class="a01 grid-element">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/art/twigs.jpg" class="g-img">
+      </a>
+
+      <a href="https://open.spotify.com/album/7mcmdIquDOA2QT8h1SSZpg?si=5dMWvVlMSGyTwlankjf8eQ" class="a02 grid-element">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/art/dsu.jpg" class="g-img">
+      </a>
+
+      <a href="https://open.spotify.com/playlist/7ELg4mOokKLSJfKYCBZLB2" class="a10 grid-element">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/art/lamp.jpg" class="g-img">
+      </a>
+
+      <a href="https://open.spotify.com/album/54OXaFd0PtGtAgauTmARkD?si=wCI1naLSQFa0OI-EQfhBKw" class="a11 grid-element">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/art/nerd.jpg" class="g-img">
+      </a>
+
+      <a href="https://open.spotify.com/album/32ium7Cxb1Xwp2MLzH2459?si=_FCCzMVvTNynIPv6pctpFg" class="a12 grid-element">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/art/ameoba.jpg" class="g-img">
+      </a>
+
+
+  </div>
+</body>
---
+++ Journal.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Journal
+*/
+
+get_header();
+
+ ?>
+ <head>
+   <!-- add fonts here -->
+   <title>DJ</title>
+ </head>
+
+<body class="journal">
+  <div class="container">
+
+
+    <!--home buton -->
+    <a href="../observations">
+      <div class="return-home return-home-journal">
+        <div class="return-home-color"></div>
+        <div class="return-home-text">Home</div>
+      </div>
+    </a>
+
+    <div class="pass">Enter super secret password:</div>
+
+
+    <div class="hints">   Hints
+      <div class="hint-1">1.</div>
+      <div class="hint-2">2.</div>
+      <div class="hint-3">3.</div>
+      <div class="hint-4">4.</div>
+
+  </div>
+</body>
